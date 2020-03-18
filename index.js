90140b815bdaca3f58146f32d694c56a7ccc19ea
AidanLincoln <aidantlincoln@gmail.com>
2020-03-18 16:17:36 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array){
+  const team = array.find(item => item.result === "W")  
+  return !!team ? team.year : undefined
+}
