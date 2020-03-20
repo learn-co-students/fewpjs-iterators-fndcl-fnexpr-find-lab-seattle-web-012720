89f1a21ddf62a4435228e8998285426e7131356c
kailanak1 <kailanakahawaii@gmail.com>
2020-03-20 13:10:46 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr){
+  const result = arr.find(function(obj) { 
+    return obj.result === "W";
+  })
+  return result ? result.year : undefined;
+}
